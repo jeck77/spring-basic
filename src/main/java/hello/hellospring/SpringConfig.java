6ccee77392c408045c485c713310fb34e80ec12b
ajm <jeck77@naver.com>
2023-03-06 20:22:13 +0900

    JPA 적용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,12 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 /**
@@ -18,9 +16,12 @@ import javax.sql.DataSource;
 public class SpringConfig {
 
 
+    EntityManager em;
     DataSource dataSource;
+
     @Autowired
-    public SpringConfig(DataSource dataSource) {
+    public SpringConfig(EntityManager em, DataSource dataSource) {
+        this.em = em;
         this.dataSource = dataSource;
     }
 
@@ -33,6 +34,7 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
